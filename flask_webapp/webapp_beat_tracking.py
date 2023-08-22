56381c72175099c311208513e68fd00fd05bc1f2
Inc-G <55004390+Inc-G@users.noreply.github.com>
2023-08-22 12:37:32 +0200

    Update webapp_beat_tracking.py

--- flask_webapp/webapp_beat_tracking.py
+++ flask_webapp/webapp_beat_tracking.py
@@ -1,19 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+
+import os
+
+import soundfile as sf
+import librosa
+
 import tensorflow as tf
 import numpy as np
 import models
 import add_clicks
 
+MODEL_NAME = 'model_epoch_300'
 
 model = models.bidirectional_model()
-_ = tf.keras.models.load_model('model_weights')
+model_save = models.bidirectional_model_for_save()
 model(np.ones((1, 10, 256)))
-model.set_weights(_.get_weights())
+model_save(np.ones((1, 10, 256)))
+
+model_save.load_weights(MODEL_NAME+'/'+MODEL_NAME)
+model.set_weights(model_save.get_weights())
+print('loaded model!')
 
 app = Flask(__name__)
 
 # Specify the folder where uploaded files will be stored
 UPLOAD_FOLDER = 'static/uploaded_songs'
+FOLDER_WITH_CLICKS = 'with_clicks'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+constant_tempo = False
 
 
 @app.route('/')
@@ -46,7 +61,7 @@ def get_beat():
     loc_clicks = 'static/'  + song_name[:-4] +'_clicks.wav'
 
     add_clicks.add_clicks(song=loc, model=model, model_passed=True, model_loc='',
-        output_name=loc_clicks, constant_tempo=True, plot=False)
+        output_name=loc_clicks, constant_tempo=False, plot=False)
     
     return "beats added successfully"
 
